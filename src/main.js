ee4296ff616bacfca422b991e3b62ff6bea102fa
Liam Niehus-Staab <lnieuhusstaab@joinhoney.com>
2021-09-13 15:36:03 -0600

    starter code + data

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ code to test modifying existing files in repo, commiting and 
+ pushing to new branch based off of variable input
+ */
+const github = require('@actions/github');
+
+
+async function main() {
+
+}
+
+main().then(() => console.log('Done')).catch((e) => console.log(e));
